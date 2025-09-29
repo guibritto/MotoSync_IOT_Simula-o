fe600385d3a0e13288f8078e64fe1f95e25a7444
guibritto <guilherme.g.britt@gmail.com>
2025-09-29 13:20:31 -0300

    Atualizando para receber os CORS

--- API_SPRINT_IOT/main.py
+++ API_SPRINT_IOT/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from db import get_connection
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # pode trocar para ["http://localhost:3000", "https://seu-frontend.com"] em produção
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Rota de teste
 @app.get("/")
